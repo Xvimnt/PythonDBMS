1377bf0721ae92fea4c721f791e447ccf771a168
xvimnt <jvmonteros98@gmail.com>
2020-12-21 22:12:56 -0600

    se añade el sevidor

--- server/server.py
+++ server/server.py
@@ -1,15 +1,17 @@
-import cgi
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import socketserver
+# Imports
 import io
 import os
 import json
+# Librerias para el servidor
+import cgi
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import socketserver
 
-class MyRequestHandler(BaseHTTPRequestHandler):
+class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
             self.send_response(400)
             self.wfile.write(bytes("",'utf-8'))
 
-myServer = HTTPServer(('localhost', 4040), MyRequestHandler)
+TytusServer = HTTPServer(('localhost', 4040), Handler)
 print("Corriendo server en puerto 4040")
-myServer.serve_forever()
+TytusServer.serve_forever()
